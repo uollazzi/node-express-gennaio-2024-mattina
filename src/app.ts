0f9ba92dd66d8011161157eb630e2dbf44f1153b
Matteo Valenzi <matteo.valenzi@outlook.com>
2024-01-22 12:59:49 +0100

    initial

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-console.log("Ciao!");
+import express, { Request, Response, NextFunction } from "express";
+import { DateTime } from "luxon";
+
+const app = express();
+const port = 3000;
+
+// middlewares
+app.use((req: Request, res: Response, next: NextFunction) => {
+    console.log("LOG:", req.method, req.url, DateTime.local().toFormat("dd/MM/yyyy HH:mm:ss"));
+    next();
+});
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Ciao!");
+});
+
+app.listen(port, () => {
+    console.log(`Server in esecuzione su http://localhost:${port}`);
+});
