4b6bc924b2e67ed0c33c7c139a12b986c7b98851
Matteo Valenzi <matteo.valenzi@outlook.com>
2024-01-24 12:57:49 +0100

    .

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express, { Request, Response, NextFunction } from "express";
 import { DateTime } from "luxon";
 import path from "node:path";
+import articoli from "./data";
 
 const app = express();
 const port = 3000;
+
+app.set("views", "./src/views");
+app.set("view engine", "hbs");
 
 // middlewares
 app.use((req: Request, res: Response, next: NextFunction) => {
@@ -14,11 +18,50 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 app.use(express.static("./public"));
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("<img src='eiffel.jpg'>");
+    res.render("index", { pageTitle: "Home Page" });
+});
+
+app.get("/chi-siamo", (req: Request, res: Response) => {
+    res.render("chi-siamo", { pageTitle: "Chi Siamo" });
+});
+
+app.get("/contatti", (req: Request, res: Response) => {
+    res.render("contatti", { pageTitle: "Contatti" });
 });
 
 app.get("/errore", (req: Request, res: Response) => {
     throw new Error("Errore simulato.");
+});
+
+app.get("/articolo/:id", (req: Request, res: Response) => {
+    const id = req.params["id"];
+    const idNumber = Number(id);
+
+    if (isNaN(idNumber)) {
+        res.status(400).send("Errore nella conversione dell'id in numero.");
+        return;
+    }
+
+    // simulazione recupero dati dal MongoDB
+    const articolo = articoli.find(x => x.id == idNumber);
+
+    if (!articolo) {
+        res.status(404).send("Articolo non trovato.");
+        return;
+    }
+
+    res.render("articolo",
+        {
+            pageTitle: "Dettaglio Articolo",
+            articolo: articolo
+        }
+    );
+});
+
+app.get("/mario", (req: Request, res: Response) => {
+    const mario = { nome: "Mario", cognome: "Rossi" };
+
+    res.json(mario);
 });
 
 // /films/popolari
